51c242f395a1480aa8f05a443bb70f5798f0ead0
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-09-22 17:18:02 +0200

    Add library typings

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Validates an input value as a spanish National Identity Number (DNI)
+ * @param value Input value to be interpreted as DNI
+ */
+export function validateDNI(value: any): FieldValidationResult;
--- src/dni.ts
+++ src/dni.ts
@@ -6,19 +6,18 @@ const defaultInvalidMessage = 'Invalid DNI';
 const isString = (input: any) => typeof input === 'string' || input instanceof String;
 const extractNumber = (dni: string): number => parseInt(dni.slice(0, dni.length - 1));
 const extractControlLetter = (dni: string): string => dni.slice(dni.length - 1).toUpperCase();
-const isValidNumber = (num: number): boolean => (num >= 1) && (num <= 99999999);
+const isValidNumber = (num: number): boolean => num >= 1 && num <= 99999999;
 const isValidControlLetter = (letter: string, num: number): boolean =>
-  controlMap.includes(letter) && (controlMap[num % 23] === letter);
+  controlMap.includes(letter) && controlMap[num % 23] === letter;
 
-
-export const validateDNI: FieldValidationFunction = (value: any, vm: any, customParams: any) => {
+export const validateDNI: FieldValidationFunction = (value: any) => {
   const result = new FieldValidationResult();
   let valid = false;
 
   if (isString(value)) {
     const num = extractNumber(value);
     const letter = extractControlLetter(value);
-    valid = (isValidNumber(num) && isValidControlLetter(letter, num));
+    valid = isValidNumber(num) && isValidControlLetter(letter, num);
   }
   result.succeeded = valid;
   result.errorMessage = valid ? '' : defaultInvalidMessage;
