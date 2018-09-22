ae5fcdfd068cf100b5159c782905160cc0b3f430
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-09-22 17:39:49 +0200

    Fix in typings

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,5 @@
  * Validates an input value as a spanish National Identity Number (DNI)
  * @param value Input value to be interpreted as DNI
  */
+import FieldValidationResult from 'lc-form-validation';
 export function validateDNI(value: any): FieldValidationResult;
