2adaf40ba9f378949046796ff6007083da7afbff
Braulio <braulio.diez@gmail.com>
2019-01-21 10:35:36 +0100

    adding declare to ts

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@
  * @param value Input value to be interpreted as DNI
  */
 import FieldValidationResult from 'lc-form-validation';
-export const VALIDATION_TYPE : string;
-export function validateDNI(value: any): FieldValidationResult;
+export declare const VALIDATION_TYPE : string;
+export declare function validateDNI(value: any): FieldValidationResult;
